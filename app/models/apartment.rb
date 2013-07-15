2a339e24e17f30d87399a1ec8a2db01219b121b1
Clovis Mello <cmello1@gmail.com>
2013-07-14 22:10:11 -0500

    Add relationship between units and apartments.

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,3 +1,5 @@
 class Apartment < ActiveRecord::Base
+  has_many :units
+
   attr_accessible :address, :address2, :city, :name, :state, :zip
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,5 @@
 class Unit < ActiveRecord::Base
+  belongs_to :apartment
+
   attr_accessible :apartment_id, :bathrooms, :bedrooms, :price, :sqft
 end
