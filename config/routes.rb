7dbb881325d55da8ef16eb0119eeea4f86f9724d
Clovis Mello <cmello1@gmail.com>
2013-07-19 22:15:44 -0500

    Redirected the root of the site to the apartment index.

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ ApartmentRater::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'apartments#index'
 
   # See how all your routes lay out with "rake routes"
 
