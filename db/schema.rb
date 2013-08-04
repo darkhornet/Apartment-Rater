af5a2647a87897c0ba95487facd00efafb2c6291
Clovis Mello <cmello1@gmail.com>
2013-08-04 16:44:08 -0500

    Make updates to get the rspec to run.

--- db/schema.rb
+++ db/schema.rb
@@ -9,36 +9,39 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130623045547) do
+ActiveRecord::Schema.define(version: 20130623045547) do
 
-  create_table "apartments", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "apartments", force: true do |t|
     t.string   "name"
     t.string   "address"
     t.string   "address2"
     t.string   "city"
     t.string   "state"
     t.string   "zip"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "categories", :force => true do |t|
+  create_table "categories", force: true do |t|
     t.string   "name"
     t.string   "type"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "units", :force => true do |t|
+  create_table "units", force: true do |t|
     t.integer  "apartment_id"
     t.integer  "bedrooms"
     t.float    "bathrooms"
     t.integer  "sqft"
     t.integer  "price"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
